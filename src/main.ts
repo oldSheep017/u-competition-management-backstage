99280972b3c5a3abcd8586601ab82682cf6cd2a3
Young <youngcoding@163.com>
2022-04-29 22:27:56 +0800

    feature: 1. 引入VueRouter 2. 引入tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { createApp } from 'vue'
+import { createApp, useAttrs } from 'vue'
 import Antd from 'ant-design-vue'
 import App from './App.vue'
 import 'ant-design-vue/dist/antd.css'
+import './styles/index.css'
+import router from './router'
 
-createApp(App)
-.use(Antd)
-.mount('#app')
+createApp(App).use(router).use(Antd).mount('#app')
---
+++ src/router/routes.ts
@@ -0,0 +1,11 @@
+import { RouteRecordRaw } from 'vue-router'
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'Login',
+    component: () => import('../views/login/index.vue')
+  }
+]
+
+export default routes
