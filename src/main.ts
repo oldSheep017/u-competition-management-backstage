c2c1c5ac0f9e336a6405d00b278ff4cd200972cc
Young <youngcoding@163.com>
2022-05-09 13:44:10 +0800

    feature: 更新主文件

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
-import { createApp, useAttrs } from 'vue'
-import Antd from 'ant-design-vue'
-import App from './App.vue'
-import 'ant-design-vue/dist/antd.css'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+// 自定义指令
+import { scrollToBottom } from './directives'
+
 import './styles/index.css'
+import Antd from 'ant-design-vue'
+import ConfigApp from './ConfigApp.vue'
+import 'ant-design-vue/dist/antd.css'
 import router from './router'
 
-createApp(App).use(router).use(Antd).mount('#app')
+const app = createApp(ConfigApp)
+
+app.directive('bottom', scrollToBottom)
+
+app.use(router).use(createPinia()).use(Antd).mount('#app')
