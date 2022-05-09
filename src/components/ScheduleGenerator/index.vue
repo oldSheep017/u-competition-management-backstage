bf8c3575d69fef463ff33d0811dae2c42faa67e2
Young <youngcoding@163.com>
2022-05-09 13:45:45 +0800

    feature: 新增赛程安排生成组件

---
+++ src/components/ScheduleGenerator/index.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+
+</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+interface IScheduleItem {
+
+}
+export default defineComponent({
+  props: {
+    value: {
+      type: Array as PropType<IScheduleItem[]>,
+      default: () => [],
+      required: true
+    }
+  },
+  emits: ['update:value'],
+  setup (props, { emit }) {
+    
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
