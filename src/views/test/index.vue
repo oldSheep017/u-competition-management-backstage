0c11b16eec505f43164e68ac31d3737938ee411c
Young <youngcoding@163.com>
2022-05-09 13:41:47 +0800

    test: 测试页面

---
+++ src/views/test/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="text-md flex">
+
+  </div>
+</template>
+
+<script lang="ts">
+import ScoreGenerator from '../../components/ScoreGenerator/index.vue'
+import SceduleGenerator from '../../components/ScheduleGenerator/index.vue'
+import { defineComponent, ref } from 'vue'
+import { IScoreItem, IScheduleItem } from '../../types/index'
+export default defineComponent({
+  components: { ScoreGenerator, SceduleGenerator },
+  setup() {
+    const text = ref({
+      name: 'Samge',
+      age: 14,
+    })
+    const data1 = ref<IScoreItem[]>([])
+    const data2 = ref<IScheduleItem[]>([])
+    return {
+      data1,
+      data2,
+      text,
+    }
+  },
+})
+</script>
