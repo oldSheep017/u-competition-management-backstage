9b60ce434f9d1e074a0444086030f68dc6b9c10d
Young <youngcoding@163.com>
2022-05-09 13:42:59 +0800

    config: 1. 路由配置

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,11 +1,28 @@
 import { RouteRecordRaw } from 'vue-router'
+import Layout from '../layout/index.vue'
 
 const routes: RouteRecordRaw[] = [
-  {
-    path: '/',
-    name: 'Login',
-    component: () => import('../views/login/index.vue')
-  }
+	{
+		path: '/login',
+		name: 'Login',
+		component: () => import('../views/login/index.vue'),
+	},
+	{
+		path: '/',
+		component: Layout,
+		children: [
+			{
+				path: '',
+				name: 'Index',
+				component: () => import('../views/index/index.vue'),
+			}
+		],
+	},
+	{
+		path: '/test',
+		name: 'Test',
+		component: () => import('../views/test/index.vue')
+	}
 ]
 
 export default routes
