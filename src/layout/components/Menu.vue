81f6de138ebb2750775d234be86b812901a00c74
Young <youngcoding@163.com>
2022-05-15 22:54:23 +0800

    enhancement: 1. 优化Menu组件 feature: 1. 新增MenuItem的配置 2. 新增切换路由时控制MenuItem的激活态

--- src/layout/components/Menu.vue
+++ src/layout/components/Menu.vue
@@ -1,24 +1,51 @@
 <template>
-  <a-menu :open-keys="keys.openKeys" v-model:selectedKeys="keys.selectedKeys">
-    <a-menu-item key="index">竞赛管理</a-menu-item>
+  <a-menu
+    :open-keys="keys.openKeys"
+    v-model:selectedKeys="keys.selectedKeys"
+    @click="checkRoute"
+  >
+    <a-menu-item key="Index">竞赛管理</a-menu-item>
+    <a-menu-item key="Create">创建比赛</a-menu-item>
+    <a-menu-item key="Staff">人员管理</a-menu-item>
   </a-menu>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from 'vue'
+import { defineComponent, reactive, watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 interface Keys {
   openKeys: string[]
   selectedKeys: string[]
 }
 export default defineComponent({
   setup() {
+    const route = useRoute()
+    const router = useRouter()
     const keys = reactive<Keys>({
       openKeys: [''],
-      selectedKeys: ['index'],
+      selectedKeys: [''],
     })
+
+    // @ts-ignore
+    keys.selectedKeys[0] = route.name
+
+    // @ts-ignore
+    const checkRoute = ({ key }) => {
+      router.push({ name: key })
+    }
+
+    watch(
+      () => route.path,
+      () => {
+        keys.selectedKeys.splice(0)
+        // @ts-ignore
+        keys.selectedKeys.push(route.name)
+      }
+    )
 
     return {
       keys,
+      checkRoute,
     }
   },
 })
