924da7f4d877fd02af1221a892d19df67e87723d
Young <youngcoding@163.com>
2022-05-15 22:55:37 +0800

    feature: 新增路由配置

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,27 +2,43 @@ import { RouteRecordRaw } from 'vue-router'
 import Layout from '../layout/index.vue'
 
 const routes: RouteRecordRaw[] = [
-	{
-		path: '/login',
-		name: 'Login',
-		component: () => import('../views/login/index.vue'),
-	},
-	{
-		path: '/',
-		component: Layout,
-		children: [
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/login/index.vue'),
+  },
+  {
+    path: '/',
+    component: Layout,
+    children: [
+      {
+        path: '',
+        name: 'Index',
+        component: () => import('../views/index/index.vue'),
+      },
 			{
-				path: '',
-				name: 'Index',
-				component: () => import('../views/index/index.vue'),
+				path: 'create',
+				name: 'Create',
+				component: () => import('../views/create/index.vue')
 			}
-		],
-	},
-	{
-		path: '/test',
-		name: 'Test',
-		component: () => import('../views/test/index.vue')
-	}
+    ],
+  },
+  // {
+  //   path: '/staff',
+  //   component: Layout,
+  //   children: [
+  //     {
+  //       path: '',
+  //       name: '',
+  //       component: () => import('../views/create/index.vue'),
+  //     },
+  //   ],
+  // },
+  {
+    path: '/test',
+    name: 'Test',
+    component: () => import('../views/test/index.vue'),
+  },
 ]
 
 export default routes
