55aec1f32d249507f3352bb7e296146584be3426
Young <youngcoding@163.com>
2022-05-15 22:52:45 +0800

    type: 增加新的公共类型定义

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,11 @@
+interface IResponse<T> {
+  code: number,
+  message: string,
+  data: T,
+  status: number,
+  status_text: string
+}
+
 interface IScoreItem {
   id: string,
   title: string,
@@ -11,7 +19,46 @@ interface IScheduleItem {
   action: string
 }
 
+interface ILoginForm {
+  username: string,
+  password: string
+}
+
+interface IRegisterForm {
+  username: string
+  password: string
+  nick_name: string
+  university: string
+  organization: string
+  phone_number: string
+}
+
+interface ObjectID {
+  $oid: string
+}
+
+interface IUserRecord {
+  _id?: ObjectID,
+  gender: number,
+  username: string,
+  password: string,
+  sign: string,
+  openid: string,
+  university: string,
+  nick_name: string,
+  uid: string,
+  avatar_url: string,
+  organization: string,
+  phone_number: string,
+  name: string,
+  __v?: number,
+}
+
 export type {
   IScoreItem,
-  IScheduleItem
+  IScheduleItem,
+  ILoginForm,
+  IRegisterForm,
+  IResponse,
+  IUserRecord
 }
